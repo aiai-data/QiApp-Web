ab81d062808799803309c41872e1e2a4d2da8678
丁当 <dingdang@luminesence.jp>
2024-12-22 11:10:17 -0500

    update netlify config

--- src/app/[lang]/layout.tsx
+++ src/app/[lang]/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
-import { locales, Locale } from '@/constants/locales'
+import { Locale } from '@/constants/locales'
 import Navigation from '@/components/Navigation'
 
 const inter = Inter({ subsets: ['latin'] })
